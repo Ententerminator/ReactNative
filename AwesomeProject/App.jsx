14f69af718667a5aa3613a05804850f1c3a05bbc
Ententerminator <zuckaboy@gmail.com>
2023-09-28 01:46:25 +0200

    added persistency

--- AwesomeProject/App.jsx
+++ AwesomeProject/App.jsx
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { DrawerToggleButton, createDrawerNavigator } from '@react-navigation/drawer';
+import { Linking, Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import HomeScreen from './src/HomeScreen';
 import Accelerometer from './src/Accelerometer';
 import Camera from './src/Camera';
@@ -17,13 +19,55 @@ import {
   Text,
   Alert,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE_V1';
+
 
 const Drawer = createDrawerNavigator();
 
 const YourApp = () => {
+
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return <ActivityIndicator />;
+  }
+
+
+
+
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    initialState={initialState}
+    onStateChange={(state) => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}>
       <Drawer.Navigator initialRouteName="Home"
       backBehavior='history'
       screenOptions={{
