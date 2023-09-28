224e48bfabf0cb10b68477199686d8b9fc8a9b2b
Ententerminator <zuckaboy@gmail.com>
2023-09-28 20:13:47 +0200

    random bild holen, aber nur einmal

--- AwesomeProject/src/RetrieveData.jsx
+++ AwesomeProject/src/RetrieveData.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import BackButton from './BackButton'; 
 import {
   StyleSheet,
@@ -6,17 +6,45 @@ import {
   View,
   SafeAreaView,
   Text,
-  Alert,
+  ActivityIndicator,
+  FlatList,
+  Image
 } from 'react-native';
 
 const RetrieveData = ({navigation}) => {
   BackButton(navigation)
 
-    return (
-    <View>
-      <Text > RetrieveData </Text>
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getDog = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const json = await response.json();
+      setData(json.message);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDog();
+  }, []);
+
+  return (
+    <View style={{flex: 1, padding: 24, justifyContent: 'center', alignItems: 'center'}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+        <Image source={{uri: data}} style={{ width: 400, height: 500, resizeMode: 'contain'}}/>
+        </View>
+
+      )}
     </View>
-    );
-  }
+  );
+};
 
   export default RetrieveData;
